419becef6ee22cf79727bab274fa9cc4ff3674b0
Max Brokman <max.brokman@vice.com>
2019-08-13 19:38:08 +0100

    Fix: Test was testing wrong class

--- tests/Unit/Metrics/AverageCalculatorTest.php
+++ tests/Unit/Metrics/AverageCalculatorTest.php
@@ -3,8 +3,9 @@
 
 namespace App\Tests\Unit\Metrics;
 
+use App\Metrics\AverageCalculator;
+use App\Metrics\MeasurementRange;
 use App\Metrics\PerformanceMeasurement;
-use App\Metrics\PerformanceStatistics;
 use Cake\Chronos\Chronos;
 use Symfony\Bundle\FrameworkBundle\Tests\TestCase;
 
@@ -12,12 +13,13 @@ class AverageCalculatorTest extends TestCase
 {
     public function testCalculate()
     {
-        $set = new PerformanceStatistics([
+        $calculator = new AverageCalculator();
+        $range = new MeasurementRange([
             new PerformanceMeasurement(Chronos::now(), 50.0),
             new PerformanceMeasurement(Chronos::now(), 100.0),
             new PerformanceMeasurement(Chronos::now(), 150.0),
         ]);
 
-        $this->assertSame(100.0, $set->getAverage());
+        $this->assertSame(100.0, $calculator->calculate($range));
     }
 }
